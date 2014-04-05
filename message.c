da0b3353d13c74a05daea2c46b7ddc870f2ec5c3
Ilya <mephi42@gmail.com>
2014-04-05 18:00:47 +0400

    standard fds are now duped to /dev/null instead of being closed

--- message.c
+++ message.c
@@ -1,6 +1,9 @@
 #include <errno.h>
+#include <fcntl.h>
 #include "message.h"
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "trace.h"
 #include <unistd.h>
 #include <zmq.h>
@@ -8,6 +11,42 @@
 static void zmq_free_wrapper(void *data, void *hint)
 {
 	free(data);
+}
+
+static int reset_std_fd(int *fd, int flags)
+{
+	int rc = -1;
+
+	int null_fd = open("/dev/null", flags);
+	if (null_fd == -1) {
+		TRACE_ERRNO("open() failed");
+		goto _out;
+	}
+
+	if (dup2(null_fd, *fd) == -1) {
+		TRACE_ERRNO("dup2(%i, %i) failed", null_fd, *fd);
+		goto _out_close_null_fd;
+	}
+
+	*fd = -1;
+	rc = 0;
+
+_out_close_null_fd:
+	if (close(null_fd) == -1)
+		TRACE_ERRNO("close(%i) failed", null_fd);
+
+_out:
+	return rc;
+}
+
+static int reset_fd(int *fd)
+{
+	if (*fd == STDIN_FILENO || *fd == STDOUT_FILENO || *fd == STDERR_FILENO)
+		return reset_std_fd(fd, (*fd == STDIN_FILENO) ? O_RDONLY : O_WRONLY);
+	if (close(*fd))
+		TRACE_ERRNO("close(%i) failed", *fd);
+	*fd = -1;
+	return 0;
 }
 
 int on_fd_readable(int *fd, void *socket, zmq_msg_t *msg, int *msg_valid, char msg_type)
@@ -40,12 +79,8 @@ int on_fd_readable(int *fd, void *socket, zmq_msg_t *msg, int *msg_valid, char m
 		*msg_valid = 1;
 		if (socket_write(socket, msg, msg_valid) == -1)
 			return -1;
-		if (count == 0) {
-			if (close(*fd))
-				TRACE_ERRNO("close(%i) failed", *fd);
-			*fd = -1;
-			return 0;
-		}
+		if (count == 0)
+			return reset_fd(fd);
 	}
 }
 
