4ba8c0e86c6a6114e1a228b930f3fcf1272f00c1
Ilya <mephi42@gmail.com>
2014-04-12 20:36:00 +0400

    zeromq3.2.3 compatibility

--- anyzmq.h
+++ anyzmq.h
@@ -4,13 +4,13 @@
 #include <zmq.h>
 
 #if ZMQ_VERSION_MAJOR == 2
-
-#define zmq_ctx_new() zmq_init(1)
-#define zmq_ctx_term(context) zmq_term(context)
-#define zmq_msg_recv(msg, socket, flags) zmq_recv(socket, msg, flags)
-#define zmq_msg_send(msg, socket, flags) zmq_send(socket, msg, flags)
-#define ZMQ_DONTWAIT ZMQ_NOBLOCK
-
+#	define zmq_ctx_new() zmq_init(1)
+#	define zmq_ctx_term(context) zmq_term(context)
+#	define zmq_msg_recv(msg, socket, flags) zmq_recv(socket, msg, flags)
+#	define zmq_msg_send(msg, socket, flags) zmq_send(socket, msg, flags)
+#	define ZMQ_DONTWAIT ZMQ_NOBLOCK
+#elif ZMQ_VERSION < ZMQ_MAKE_VERSION(3, 3, 0)
+#	define zmq_ctx_term(context) zmq_ctx_destroy(context)
 #endif
 
 #endif
